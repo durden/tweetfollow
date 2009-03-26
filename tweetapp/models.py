ddaa56f4f39fccd1240117268922733633d3ab93
Luke Lee <durdenmisc@gmail.com>
2009-03-25 21:00:36 -0500

    Adding rough draft of follower and user models. - Removed django basic auth b/c not using it anymore

--- tweetapp/models.py
+++ tweetapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+#FIXME: Some notable problems with this implementation:
+#		1. We can't detect ALL follower changes (specific users, etc), only changes in the overall
+#		   follower count trigger a 'save'.
+
+class TwitterUser(models.Model):
+	username = models.CharField(max_length=50)
+	last_visited = models.DateTimeField(auto_now=True)
+
+class FollowerChange(models.Model):
+	user = models.ForeignKey(TwitterUser)
+	type = models.BooleanField()
+	time = models.DateTimeField(auto_now=True)
+	count = models.IntegerField()
+
+	# Store tweets here b/c doesn't seem like a reason to just retrieve the tweets without
+	# the follower change
+	tweet1 = models.CharField(max_length=140)
+	tweet2 = models.CharField(max_length=140)
+	tweet3 = models.CharField(max_length=140)
+	tweet4 = models.CharField(max_length=140)
+	tweet5 = models.CharField(max_length=140)
+	# FIXME: Maybe find an actual list type?
+	# FIXME: Store the usernames of followers
