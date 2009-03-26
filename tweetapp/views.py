562fe8471a09eb7f6e20ae214df09954bcbe8966
Luke Lee <durdenmisc@gmail.com>
2009-03-25 22:31:00 -0500

    Hooking up logic to determine if number of followers change since last visit and print results in home template

--- tweetapp/views.py
+++ tweetapp/views.py
@@ -24,12 +24,45 @@ def login(request):
 		except (twitterapi.TwitterError, HTTPError):
 			return render_to_response('login.html', {'msg' : 'Twitter user/password incorrect'})
 
-		user_obj = TwitterUser(username=user)
+		user_obj = TwitterUser.objects.get(username=user)
+		change_obj = None
+		cnt = len(api.GetFollowers())
+
+		# Update last_visited and follower_count
+		if user_obj:
+			#FIXME: update last_visited
+			if cnt != user_obj.follow_count:
+				change_obj = FollowerChange()	
+				change_obj.user = user_obj
+
+				# FIXME Probably some cool python trick to do this in 1 line
+				recent_tweets = api.GetUserTimeline(count=5)
+				change_obj.tweet1 = recent_tweets[0].text
+				change_obj.tweet2 = recent_tweets[1].text
+				change_obj.tweet3 = recent_tweets[2].text
+				change_obj.tweet4 = recent_tweets[3].text
+				change_obj.tweet5 = recent_tweets[4].text
+
+				follow_diff = user_obj.follow_count - cnt
+				change_obj.count = abs(follow_diff)
+
+				# Gained followers
+				if follow_diff >= 1:
+					change_obj.type = True
+				else:
+					change_obj.type = False
+
+				# FIXME: exception?
+				change_obj.save()
+		else:
+			user_obj = TwitterUser(username=user)
+
+		user_obj.follow_count = cnt
 
 		# FIXME: Exception to catch?
 		user_obj.save()
 
-		return render_to_response('login.html', {'msg' : 'Added %s!' % (user)})
+		return render_to_response('home.html', {'user' : user_obj, 'follow' : change_obj})
 
 	return render_to_response('login.html')
 
