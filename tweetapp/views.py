9e968884335195cd65c3faf5e9be60ea5c56267b
Luke Lee <durdenmisc@gmail.com>
2009-04-14 23:00:52 -0500

    Removing the FollwerChange model. - It is redundant and the new FollowerPair is better - Updated home template to use followerpair model

--- tweetapp/views.py
+++ tweetapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render_to_response
-from tweetapp.models import TwitterUser, FollowerChange, FollowPair
+from tweetapp.models import TwitterUser, FollowPair
 from urllib2 import HTTPError
 
 import twitterapi
@@ -29,7 +29,6 @@ def login(request):
                         {'msg' : 'Twitter user/password incorrect'})
 
     user_obj = None
-    change_obj = None
     followers = api.GetFollowers()
     cnt = len(followers)
 
@@ -37,30 +36,6 @@ def login(request):
     if TwitterUser.objects.filter(username=user):
         user_obj = TwitterUser.objects.get(username=user)
 
-        #FIXME: update last_visited
-        if cnt != user_obj.follow_count:
-            change_obj = FollowerChange()	
-            change_obj.user = user_obj
-
-            # FIXME Probably some cool python trick to do this in 1 line
-            recent_tweets = api.GetUserTimeline(count=5)
-            change_obj.tweet1 = recent_tweets[0].text
-            change_obj.tweet2 = recent_tweets[1].text
-            change_obj.tweet3 = recent_tweets[2].text
-            change_obj.tweet4 = recent_tweets[3].text
-            change_obj.tweet5 = recent_tweets[4].text
-
-            follow_diff = user_obj.follow_count - cnt
-            change_obj.count = abs(follow_diff)
-
-            # Gained followers
-            if follow_diff >= 1:
-                change_obj.type = True
-            else:
-                change_obj.type = False
-
-            # FIXME: exception?
-            change_obj.save()
     else:
         user_obj = TwitterUser(username=user)
 
@@ -69,15 +44,17 @@ def login(request):
     # FIXME: Exception to catch?
     user_obj.save()
 
+    pairs = []
+
     # save followers
     for follower in followers:
         followpair = FollowPair()
-        followpair.user = TwitterUser(username=user)
+        followpair.user = user_obj
         followpair.followerid = follower.id
         followpair.save()
-    
-    return render_to_response('home.html',
-                        {'user' : user_obj, 'follow' : change_obj})
+        pairs.append(followpair)
+
+    return render_to_response('home.html', {'followers' : pairs})
 
 def users(request):
     usrs = User.objects.all()
