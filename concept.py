26094a59771a5e770573bdb2e397cb0edd24306c
Luke Lee <durdenmisc@gmail.com>
2009-06-29 21:07:53 -0500

    Tabs to spaces, etc.

--- concept.py
+++ concept.py
@@ -9,41 +9,47 @@ import twitterapi
 #	   who started the script.
 
 if len(sys.argv) <= 1:
-	sys.exit("Please specify your twitter username")
+    sys.exit("Please specify your twitter username")
 
 user = sys.argv[1]
 pwd = getpass.getpass()
 
 if len(pwd) == 0:
-	sys.exit("Please specify your twitter password")
+    sys.exit("Please specify your twitter password")
 
 api = twitterapi.Api(username=user, password=pwd)
 
 try:
-	while 1:
-		followers1 = set(api.GetFollowers())
+    while 1:
+        followers1 = set(api.GetFollowers())
 
-		if not len(followers1):
-			print "You don't have any friends yet?!"
+        if not len(followers1):
+            print "You don't have any friends yet?!"
 
-		# FIXME: Sleep will have to be much longer if you want to run
-        #		 this for longer than a few minutes.
-		time.sleep(30)
-	
-		followers2 = set(api.GetFollowers())
+        ii = 0
+        for fol in followers1:
+            print fol.screen_name
+            ii = ii + 1
+        print ii
+        break
+        # FIXME: Sleep will have to be much longer if you want to run
+        # this for longer than a few minutes.
+        #time.sleep(30)
 
-		if len(followers1) > len(followers2):
-			missing = followers1 - followers2
+        #followers2 = set(api.GetFollowers())
 
-			for usr in missing:
-				print usr.name, " hates you now"
+        #if len(followers1) > len(followers2):
+        #	missing = followers1 - followers2
 
-			print "What you said to piss them off:"
-			# FIXME: This just gets the last 20 tweets probably want
-			#		 get the ones since the first followers were found
-			tweets = api.GetUserTimeline()
-			for tweet in tweets:
-				print tweet.created_at, " -- ", tweet.text
+        #for usr in missing:
+            #	print usr.name, " hates you now"
+
+            #print "What you said to piss them off:"
+            # FIXME: This just gets the last 20 tweets probably want
+            #		 get the ones since the first followers were found
+            #tweets = api.GetUserTimeline()
+            #for tweet in tweets:
+            #	print tweet.created_at, " -- ", tweet.text
 
 except KeyboardInterrupt:
-	pass
+    pass
