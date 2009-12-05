ccce7f1b445ea04de340dfd2203b1a6e315c9708
Luke Lee <durdenmisc@gmail.com>
2009-12-04 23:19:57 -0600

    Replaced FollowPair with Followers -- don't need added/removed time anymore

--- tweetapp/models.py
+++ tweetapp/models.py
@@ -2,10 +2,7 @@ from django.db import models
 
 class TwitterUser(models.Model):
     username = models.CharField(max_length=50)
-    lastlogin = models.DateTimeField(null=True)
 
-class FollowPair(models.Model):
+class Followers(models.Model):
     user        = models.ForeignKey(TwitterUser, to_field='username')
     follower    = models.CharField(max_length=50)
-    added       = models.DateTimeField(auto_now_add=True)
-    removed     = models.DateTimeField(null=True)
