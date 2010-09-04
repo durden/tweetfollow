c7edf714e4992280fd2912fd8ae79396102e741f
Luke Lee <durdenmisc@gmail.com>
2010-09-04 14:42:03 -0500

    Added urls from previous django project, updated settings, etc.

--- urls.py
+++ urls.py
@@ -1,23 +1,27 @@
-# Copyright 2008 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+from django.views.generic.simple import direct_to_template
 from django.conf.urls.defaults import *
+from tweetapp.views import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
-    # (r'^foo/', include('foo.urls')),
+    # (r'^tweetfollow/', include('tweetfollow.foo.urls')),
 
-    # Uncomment this for admin:
-#     (r'^admin/', include('django.contrib.admin.urls')),
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/(.*)', admin.site.root),
+    (r'^$', register),
+    (r'^register/$', register),
+    (r'^about/$', direct_to_template, {'template': 'about.html'}),
+    (r'^users/$', users),
+    (r'^refresh/(.*)', update_followers),
+    (r'^update/(.*)', update_followers),
+    (r'^update_all/$', update_all),
+    (r'^success/$', direct_to_template, {'template': 'success.html'}),
 )
